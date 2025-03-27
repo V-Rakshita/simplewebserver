b0a621a90a913561d6040b78917d4c0e78fb2f6d
V-Rakshita <rakshi010606@gmail.com>
2025-03-27 11:27:16 +0530

    Success

--- simplewebserver.py
+++ simplewebserver.py
@@ -1,30 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date: 27.03.2025
-## Name: V. Rakshita
-## Register number: 212224100049 
-
-## AIM:
-To develop a simple webserver to serve html pages and display the list of protocols in TCP/IP Protocol Suite.
-
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content='''
@@ -62,10 +35,3 @@ print("This is my webserver")
 server_address =('',5000)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-
-## OUTPUT:
-![alt text](<WEB EXP 1.png>)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
